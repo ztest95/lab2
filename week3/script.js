21fb62e6f4b08d1717689d8830258b6486a43ab5
ztest95 <ztestgithub+git@gmail.com>
2024-01-04 15:37:51 +0800

    Real Circle + Js Rotation

--- week3/script.js
+++ week3/script.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    let realCircle = document.querySelector('#real-circle');
 
-    alert('test')
+    window.onscroll = function() {
+        let scrollPercentage = calculateScrollPercentage();
+        rotateCircle(realCircle, scrollPercentage);
+    };
 });
+
+function calculateScrollPercentage() {
+    let scrollTop =  document.documentElement.scrollTop;
+    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    return (scrollTop / scrollHeight) * 100;
+}
+
+function rotateCircle(element, percentage) {
+    let rotationDegrees = (percentage / 100) * 360;
+    element.style.transform = `rotate(${rotationDegrees}deg)`;
+}
