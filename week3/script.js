63cd33b30ff4608a68913e1614213fb2b3446362
ztest95 <ztestgithub+git@gmail.com>
2024-01-06 16:53:50 +0800

    Media Query, Mobile Responsiveness, Footer

--- week3/script.js
+++ week3/script.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    let realCircle = document.querySelector('#real-circle');
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     let section3 = document.querySelector('#section-3');
 
-    window.onscroll = function() {
-        let scrollPercentage = calculateScrollPercentage();
-        rotateCircle(realCircle, scrollPercentage);
-    };
-});
+//     window.onscroll = function() {
+//         let scrollPercentage = calculateScrollPercentage();
+//         rotateCircle(section3, scrollPercentage);
+//     };
+// });
 
-function calculateScrollPercentage() {
-    let scrollTop =  document.documentElement.scrollTop;
-    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    return (scrollTop / scrollHeight) * 100;
-}
+// function calculateScrollPercentage() {
+//     let scrollTop =  document.documentElement.scrollTop;
+//     let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//     return (scrollTop / scrollHeight) * 100;
+// }
 
-function rotateCircle(element, percentage) {
-    let rotationDegrees = (percentage / 100) * 360;
-    element.style.transform = `rotate(${rotationDegrees}deg)`;
-}
+// function rotateCircle(element, percentage) {
+//     let rotationDegrees = (percentage / 100) * 360;
+//     element.style.setProperty('--rotation', rotationDegrees + 'deg');
+// }
