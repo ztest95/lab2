07165b67e19e6ef1f808f1ba643b6abae4a6c4bc
ztest95 <ztestgithub+git@gmail.com>
2024-01-11 01:05:53 +0800

    Add week5

---
+++ week5/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let circle2= document.querySelector('#circle-2');
+
+    window.onscroll = function() {
+        let scrollPercentage = calculateScrollPercentage();
+        rotateCircle(circle2, scrollPercentage);
+    };
+
+    console.log("Hello visitor")
+
+    let amogus = document.querySelector('#amogusyellow');
+    amogus.addEventListener('click', function(event) {
+        alert('sus');
+    });
+});
+
+function calculateScrollPercentage() {
+    let scrollTop =  document.documentElement.scrollTop; // distance from top of page to top of viewport
+    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight; // total height of page
+    return (scrollTop / scrollHeight) * 100; // how much of the page is scrolled
+}
+
+function rotateCircle(element, percentage) {
+    let rotationDegrees = (percentage / 100) * 360;
+    element.style.setProperty('--rotation', rotationDegrees + 'deg'); // sets css variable '--rotation' to rotationDegrees
+}
