4cd22febec697be00e45fa669cb3bf1fc92a26ed
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-01-23 01:21:47 +0000

    Javascript: Modules

--- week5/script.js
+++ week5/script.js
@@ -1,3 +1,6 @@
+// Javascript modules
+import loadColorChanger from "./colorchanger.js";
+
 document.addEventListener("DOMContentLoaded", function(event) {
     let circle2= document.querySelector('#circle-2');
 
@@ -29,42 +32,3 @@ function rotateCircle(element, percentage) {
     let rotationDegrees = (percentage / 100) * 360;
     element.style.setProperty('--rotation', rotationDegrees + 'deg'); // sets css variable '--rotation' to rotationDegrees
 }
-
-function loadColorChanger(element) {
-    
-    const colorsDict = {
-        'Yellow': '#fff01f',
-        'Lime': '#39FF14',
-        'Cyan': 'cyan'
-    }
-    // Javascript Arrays
-    const keys = Object.keys(colorsDict)
-    // Javascript Variables, Let, Const & Data Types
-    let index = 0
-
-    // Javascript Events
-    element.addEventListener('click', function(event) {
-        // Javascript Operators, Assignment, Arithmetic & Math
-        new_index = Math.floor(Math.random() * keys.length);
-
-        if (new_index == index) {
-            new_index = (new_index + 1) % keys.length;
-        }
-        index = new_index
-
-        // Change Webpage Title
-        const title = document.querySelector('title');
-        title.innerHTML = keys[index] + ' Potato' // Javascript Strings
-
-        // Change Title Name
-        element.innerHTML = keys[index]
-
-        // Change Website Color
-        document.documentElement.style.setProperty('--highlight', colorsDict[keys[index]]);
-
-        //
-        const amogus = document.querySelector('#img-amogus');
-        amogus.src = './media/sus-' + keys[index] + '.webp';
-    });
-
-};
