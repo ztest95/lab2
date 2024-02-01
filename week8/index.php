379e570a3956d4491904ca6c132fda1e7f1f61e6
ztest95 <ztestgithub+git@gmail.com>
2024-02-01 15:31:40 +0800

    Add php form and some styling

--- week8/index.php
+++ week8/index.php
@@ -199,7 +199,9 @@
         <h1>Score: </h1><h1 id='score-counter' style="text-alignment: center;"></h1>
     </div>
     
-    <section>
+    <section id="form" class="col">
+    
+        <div>
         <?php
         // define variables and set to empty values
         $nameErr = $emailErr = $genderErr = $websiteErr = "";
@@ -256,32 +258,30 @@
         return $data;
         }
         ?>
+            <h2>PHP Form Validation Example</h2>
+            <p><span class="error">* required field</span></p>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            Name: <input type="text" name="name" value="<?php echo $name;?>">
+            <span class="error">* <?php echo $nameErr;?></span>
+            <br><br>
+            E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+            <span class="error">* <?php echo $emailErr;?></span>
+            <br><br>
+            Website: <input type="text" name="website" value="<?php echo $website;?>">
+            <span class="error"><?php echo $websiteErr;?></span>
+            <br><br>
+            Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+            <br><br>
+            Gender:
+            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other
+            <span class="error">* <?php echo $genderErr;?></span>
+            <br><br>
+            <input type="submit" name="submit" value="Submit">
+            </form>
 
-
-        <h2>PHP Form Validation Example</h2>
-        <p><span class="error">* required field</span></p>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-        Name: <input type="text" name="name" value="<?php echo $name;?>">
-        <span class="error">* <?php echo $nameErr;?></span>
-        <br><br>
-        E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-        <span class="error">* <?php echo $emailErr;?></span>
-        <br><br>
-        Website: <input type="text" name="website" value="<?php echo $website;?>">
-        <span class="error"><?php echo $websiteErr;?></span>
-        <br><br>
-        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-        <span class="error">* <?php echo $genderErr;?></span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">  
-        </form>
-
-        <?php
+            <?php
         echo "<h2>Your Input:</h2>";
         echo $name;
         echo "<br>";
@@ -293,6 +293,9 @@
         echo "<br>";
         echo $gender;
         ?>
+        </div>
+
+       
     </section>
 
     <div id="up-arrow">
