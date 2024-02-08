d8e716d925e2abb66bff0714a1892a85a63eed20
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-02-08 07:38:50 +0000

    last

--- week8/index.php
+++ week8/index.php
@@ -203,64 +203,61 @@
 
         <div>
             <?php
-            // define variables and set to empty values
-            $nameErr = $emailErr = $genderErr = $websiteErr = "";
-            $name = $email = $gender = $comment = $website = "";
-
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty($_POST["name"])) {
-                $nameErr = "Name is required";
-            } else {
-                $name = test_input($_POST["name"]);
-                // check if name only contains letters and whitespace
-                if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-                $nameErr = "Only letters and white space allowed";
+                if (empty($_POST["name"])) {
+                    $nameErr = "Name is required";
+                } else {
+                    $name = test_input($_POST["name"]);
+                    // check if name only contains letters and whitespace
+                    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                    $nameErr = "Only letters and white space allowed";
+                    }
                 }
-            }
-            
-            if (empty($_POST["email"])) {
-                $emailErr = "Email is required";
-            } else {
-                $email = test_input($_POST["email"]);
-                // check if e-mail address is well-formed
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
-                }
-            }
                 
-            if (empty($_POST["website"])) {
-                $website = "";
-            } else {
-                $website = test_input($_POST["website"]);
-                // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-                if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-                $websiteErr = "Invalid URL";
+                if (empty($_POST["email"])) {
+                    $emailErr = "Email is required";
+                } else {
+                    $email = test_input($_POST["email"]);
+                    // check if e-mail address is well-formed
+                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $emailErr = "Invalid email format";
+                    }
                 }
-            }
+                    
+                if (empty($_POST["website"])) {
+                    $website = "";
+                } else {
+                    $website = test_input($_POST["website"]);
+                    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+                    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+                    $websiteErr = "Invalid URL";
+                    }
+                }
 
-            if (empty($_POST["comment"])) {
-                $comment = "";
-            } else {
-                $comment = test_input($_POST["comment"]);
-            }
+                if (empty($_POST["comment"])) {
+                    $comment = "";
+                } else {
+                    $comment = test_input($_POST["comment"]);
+                }
 
-            if (empty($_POST["gender"])) {
-                $genderErr = "Gender is required";
-            } else {
-                $gender = test_input($_POST["gender"]);
-            }
-            }
+                if (empty($_POST["gender"])) {
+                    $genderErr = "Gender is required";
+                } else {
+                    $gender = test_input($_POST["gender"]);
+                }
+                }
 
-            function test_input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-            }
-            ?>
-                <h2>PHP Form Validation Example qweqwe</h2>
+                function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+                }
+                ?>
+
+                <h2>PHP Form Validation Example</h2>
                 <p><span class="error">* required field</span></p>
-                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
                 Name: <input type="text" name="name" value="<?php echo $name;?>">
                 <span class="error">* <?php echo $nameErr;?></span>
                 <br><br>
@@ -275,50 +272,62 @@
                 Gender:
                 <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
                 <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-                <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other
+                <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
                 <span class="error">* <?php echo $genderErr;?></span>
                 <br><br>
-                <input type="submit" name="submit" value="Submit">
+                <input type="submit" name="submit" value="Submit">  
                 </form>
 
                 <?php
-                    echo "<h2>Your Input:</h2>";
-                    echo $name;
-                    echo "<br>";
-                    echo $email;
-                    echo "<br>";
-                    echo $website;
-                    echo "<br>";
-                    echo $comment;
-                    echo "<br>";
-                    echo $gender;
+                echo "<h2>Your Input:</h2>";
+                echo $name;
+                echo "<br>";
+                echo $email;
+                echo "<br>";
+                echo $website;
+                echo "<br>";
+                echo $comment;
+                echo "<br>";
+                echo $gender;
                 ?>
 
                 <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $servername = "localhost";
-                        $username = "webprogss221";
-                        $password = "=latHen97";
-                        $dbname = "webprogss221";
 
-                        // Create connection
-                        $conn = new mysqli($servername, $username, $password, $dbname);
-                        // Check connection
-                        if ($conn->connect_error) {
-                        die("Connection failed: " . $conn->connect_error);
-                        }
+                if ($_SERVER["REQUEST_METHOD"] == "POST") 
+                {
 
-                        $sql = "INSERT INTO mctapiru_myguests (name, email, website, command, gender)
-                        VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+                // For Xampp Localhost
+                //$servername = "localhost";
+                //$username = "root";
+                //$password = "";
+                //$dbname = "myDB";
 
-                        if ($conn->query($sql) === TRUE) {
-                        echo "New record created successfully";
-                        } else {
-                        echo "Error: " . $sql . "<br>" . $conn->error;
-                        }
+                // For apcwebprog
+                $servername = "localhost";
+                $username = "webprogss221";
+                $password = "=latHen97";
+                $dbname = "webprogss221";
 
-                    $conn->close();
-                    }
+
+
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "INSERT INTO mctapiru_myguests (name, email,website,comment,gender)
+                VALUES ('$name', '$email','$website','$comment','$gender')";
+
+                if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+                } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+
+                $conn->close();
+                }
                 ?>
         </div>
 
