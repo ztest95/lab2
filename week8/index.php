2c7c29639dda3f950be723c2eb7b4d4f74f617ae
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-02-08 07:13:24 +0000

    fix form

--- week8/index.php
+++ week8/index.php
@@ -202,21 +202,6 @@
     <section class="col" id="form">
 
         <?php
-        $servername = "localhost";
-        $username = "webprogss221";
-        $password = "=latHen97";
-        $dbname = "webprogss221";
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
-
-        ?>
-
-        <?php
         // define variables and set to empty values
         $nameErr = $emailErr = $genderErr = $websiteErr = "";
         $name = $email = $gender = $comment = $website = "";
@@ -265,14 +250,28 @@
             }
             
             if ($nameErr == "" && $emailErr == "" && $websiteErr == "" && $genderErr == "") {
-                $stmt = $conn->prepare("INSERT INTO mctapiru_myguests (name, email, website, comment, gender) VALUES (?, ?, ?, ?, ?)");
-                $stmt->bind_param("sssss", $name, $email, $website, $comment, $gender);
-            
-                if ($stmt->execute() === TRUE) {
-                    echo "New record created successfully";
-                } else {
-                    echo "Error: " . $stmt->error;
+                $servername = "localhost";
+                $username = "webprogss221";
+                $password = "=latHen97";
+                $dbname = "webprogss221";
+        
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
                 }
+        
+                $sql = "INSERT INTO mctapiru_myguests (name, email,website,comment,gender)
+                VALUES ('$name', '$email','$website','$comment','$gender')";
+        
+                if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+                } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+        
+                $conn->close();
             }
         }
 
@@ -321,6 +320,8 @@
         echo "<br>";
         echo $gender;
         ?>
+
+
 
     </section>
 
