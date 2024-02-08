b8a0dd99967283a5ca2c0ea59e75d00148aff7a0
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-02-08 06:49:17 +0000

    form update

--- week8/index.php
+++ week8/index.php
@@ -200,53 +200,80 @@
     </div>
     
     <section>
+
+        <?php
+        $servername = "localhost";
+        $username = "webprogss221";
+        $password = "=latHen97";
+        $dbname = "webprogss221";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        ?>
+
         <?php
         // define variables and set to empty values
         $nameErr = $emailErr = $genderErr = $websiteErr = "";
         $name = $email = $gender = $comment = $website = "";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
-        } else {
-            $name = test_input($_POST["name"]);
-            // check if name only contains letters and whitespace
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-            $nameErr = "Only letters and white space allowed";
+            if (empty($_POST["name"])) {
+                $nameErr = "Name is required";
+            } else {
+                $name = test_input($_POST["name"]);
+                // check if name only contains letters and whitespace
+                if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                $nameErr = "Only letters and white space allowed";
+                }
             }
-        }
-        
-        if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
-        } else {
-            $email = test_input($_POST["email"]);
-            // check if e-mail address is well-formed
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email format";
-            }
-        }
             
-        if (empty($_POST["website"])) {
-            $website = "";
-        } else {
-            $website = test_input($_POST["website"]);
-            // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-            $websiteErr = "Invalid URL";
+            if (empty($_POST["email"])) {
+                $emailErr = "Email is required";
+            } else {
+                $email = test_input($_POST["email"]);
+                // check if e-mail address is well-formed
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
+                }
             }
-        }
+                
+            if (empty($_POST["website"])) {
+                $website = "";
+            } else {
+                $website = test_input($_POST["website"]);
+                // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+                if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+                $websiteErr = "Invalid URL";
+                }
+            }
 
-        if (empty($_POST["comment"])) {
-            $comment = "";
-        } else {
-            $comment = test_input($_POST["comment"]);
-        }
+            if (empty($_POST["comment"])) {
+                $comment = "";
+            } else {
+                $comment = test_input($_POST["comment"]);
+            }
 
-        if (empty($_POST["gender"])) {
-            $genderErr = "Gender is required";
-        } else {
-            $gender = test_input($_POST["gender"]);
-        }
+            if (empty($_POST["gender"])) {
+                $genderErr = "Gender is required";
+            } else {
+                $gender = test_input($_POST["gender"]);
+            }
+            
+            if ($nameErr == "" && $emailErr == "" && $websiteErr == "" && $genderErr == "") {
+                $stmt = $conn->prepare("INSERT INTO mctapiru_myguests (name, email, website, comment, gender) VALUES (?, ?, ?, ?, ?)");
+                $stmt->bind_param("sssss", $name, $email, $website, $comment, $gender);
+            
+                if ($stmt->execute() === TRUE) {
+                    echo "New record created successfully";
+                } else {
+                    echo "Error: " . $stmt->error;
+                }
+            }
         }
 
         function test_input($data) {
@@ -255,8 +282,10 @@
         $data = htmlspecialchars($data);
         return $data;
         }
-        ?>
 
+        $conn->close();
+        ?>
+    
 
         <h2>PHP Form Validation Example</h2>
         <p><span class="error">* required field</span></p>
@@ -293,6 +322,7 @@
         echo "<br>";
         echo $gender;
         ?>
+
     </section>
 
     <div id="up-arrow">
