cd84fefeb5ae255bc8e5960a04f3ab23bd3dce24
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-02-08 07:21:33 +0000

    q

--- week8/index.php
+++ week8/index.php
@@ -201,12 +201,13 @@
     
     <section class="col" id="form">
 
-        <?php
-        // define variables and set to empty values
-        $nameErr = $emailErr = $genderErr = $websiteErr = "";
-        $name = $email = $gender = $comment = $website = "";
+        <div>
+            <?php
+            // define variables and set to empty values
+            $nameErr = $emailErr = $genderErr = $websiteErr = "";
+            $name = $email = $gender = $comment = $website = "";
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST["name"])) {
                 $nameErr = "Name is required";
             } else {
@@ -248,78 +249,51 @@
             } else {
                 $gender = test_input($_POST["gender"]);
             }
-            
-            if ($nameErr == "" && $emailErr == "" && $websiteErr == "" && $genderErr == "") {
-                $servername = "localhost";
-                $username = "webprogss221";
-                $password = "=latHen97";
-                $dbname = "webprogss221";
-        
-                // Create connection
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                // Check connection
-                if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-                }
-        
-                $sql = "INSERT INTO mctapiru_myguests (name, email,website,comment,gender)
-                VALUES ('$name', '$email','$website','$comment','$gender')";
-        
-                if ($conn->query($sql) === TRUE) {
-                echo "New record created successfully";
-                } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-        
-                $conn->close();
             }
-        }
 
-        function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-        }
+            function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+            }
+            ?>
+                <h2>PHP Form Validation Example</h2>
+                <p><span class="error">* required field</span></p>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                Name: <input type="text" name="name" value="<?php echo $name;?>">
+                <span class="error">* <?php echo $nameErr;?></span>
+                <br><br>
+                E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+                <span class="error">* <?php echo $emailErr;?></span>
+                <br><br>
+                Website: <input type="text" name="website" value="<?php echo $website;?>">
+                <span class="error"><?php echo $websiteErr;?></span>
+                <br><br>
+                Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+                <br><br>
+                Gender:
+                <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+                <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+                <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other
+                <span class="error">* <?php echo $genderErr;?></span>
+                <br><br>
+                <input type="submit" name="submit" value="Submit">
+                </form>
 
-        ?>
-    
-
-        <h2>PHP Form Validation Example</h2>
-        <p><span class="error">* required field</span></p>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-        Name: <input type="text" name="name" value="<?php echo $name;?>">
-        <span class="error">* <?php echo $nameErr;?></span>
-        <br><br>
-        E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-        <span class="error">* <?php echo $emailErr;?></span>
-        <br><br>
-        Website: <input type="text" name="website" value="<?php echo $website;?>">
-        <span class="error"><?php echo $websiteErr;?></span>
-        <br><br>
-        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-        <span class="error">* <?php echo $genderErr;?></span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">  
-        </form>
-
-            <?php
-        echo "<h2>Your Input:</h2>";
-        echo $name;
-        echo "<br>";
-        echo $email;
-        echo "<br>";
-        echo $website;
-        echo "<br>";
-        echo $comment;
-        echo "<br>";
-        echo $gender;
-        ?>
+                <?php
+            echo "<h2>Your Input:</h2>";
+            echo $name;
+            echo "<br>";
+            echo $email;
+            echo "<br>";
+            echo $website;
+            echo "<br>";
+            echo $comment;
+            echo "<br>";
+            echo $gender;
+            ?>
+        </div>
 
 
 
